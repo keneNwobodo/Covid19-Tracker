296943498d752cd7ac7e5d3e4da5de5d20fb2796
kenenwobodo <nwobodokenechukwu2@gmail.com>
2020-11-05 00:29:08 +0100

    covid app update

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { Cards, Chart, CountryPicker} from "./components";
+import { fetchData } from "./api";
+
+import styles from "./App.module.css";
+
+class App extends Component {
+  state = {
+    data: {},
+    country: "",
+
+  }
+
+ async componentDidMount(){
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData});
+  }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+
+    console.log(fetchedData);
+
+    this.setState({ data: fetchedData, country: country});
+  }
+
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+        <div className={styles.container}>
+          <img className={styles.image}  />
+            <Cards data={data} />
+            <CountryPicker handleCountryChange={this.handleCountryChange} />
+            <Chart data={data} country={country} />
+        </div>   
+    );
+  }
+  
+}
+
+export default App;
